e4db2bc8dcf36bc7ec95e5f643066f8a47b68c5c
Han, Sangyoen <coyomy@naver.com>
2020-05-06 17:10:36 +0900

    중간시험1

--- app/src/main/java/com/example/midexam/Exam3Activity.java
+++ app/src/main/java/com/example/midexam/Exam3Activity.java
@@ -1,13 +1,19 @@
 package com.example.midexam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class Exam3Activity extends AppCompatActivity {
 
 
-    RecyclerView3Adapter recyclerView3Adapter;
+   Exam3Adapter Exam3Adapter;
     ArrayList<Memo> arrayList;
 
     @Override
@@ -16,15 +22,14 @@ public class Exam3Activity extends AppCompatActivity {
         setContentView(R.layout.activity_exam3);
 
         arrayList = new ArrayList<Memo>();
-        arrayList.add(new Memo("one", new Date()));
-        arrayList.add(new Memo("two", new Date()));
-
-        recyclerView3Adapter = new RecyclerView3Adapter(this, arrayList);
+        arrayList.add(new Memo("one"));
+        arrayList.add(new Memo("two"));
+        Exam3Adapter= new Exam3Adapter(this, arrayList);
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(recyclerView3Adapter);
+        recyclerView.setAdapter(Exam3Adapter);
 
     }
 }
--- app/src/main/java/com/example/midexam/Exam3EditActivity.java
+++ app/src/main/java/com/example/midexam/Exam3EditActivity.java
@@ -2,13 +2,43 @@ package com.example.midexam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Exam3EditActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam3_edit);
+
+        Button button = (Button) findViewById(R.id.button7);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText_title = (EditText) findViewById(R.id.editText2);
+                String title = editText_title.getText().toString();
+                if (isEmptyOrWhiteSpace(title)) {
+                    editText_title.setError("내용을 입력하세요");
+                    return;
+                }
+
+
+
+                Memo memo = new Memo(title);
+                Intent intent = new Intent();
+                intent.putExtra("MEMO", memo);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        };
+        button.setOnClickListener(listener);
+    }
+
+    static boolean isEmptyOrWhiteSpace(String s) {
+        if (s == null) return true;
+        return s.toString().trim().length() == 0;
     }
 }
